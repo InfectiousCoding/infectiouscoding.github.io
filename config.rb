8ea3a21cc2af4d37775ae0ee358ab87b47ea4f40
gogromat <gogromat@gmail.com>
2014-02-18 22:14:37 -0500

    Fixes

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/infectiouscoding/"
-http_images_path = "/infectiouscoding/images"
+http_path = "/"
+http_images_path = "/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/infectiouscoding/fonts"
-css_dir = "public/infectiouscoding/stylesheets"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
